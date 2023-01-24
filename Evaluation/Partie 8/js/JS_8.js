d9579da9d2fe90d06697f14ddb6699eb17ed008a
GuillaumeC96 <122291329+GuillaumeC96@users.noreply.github.com>
2023-01-24 11:12:33 +0100

    Delete Evaluation/Partie 8 directory

--- Evaluation/Partie 8/js/JS_8.js
+++
@@ -1,9 +0,0 @@
-alert("Exercice 3");
-
-var A = prompt("Choisir le prénom","");
-
-var tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
-
-const result = tab.filter(tab => tab.length > 6);
-
-alert(tabl);
