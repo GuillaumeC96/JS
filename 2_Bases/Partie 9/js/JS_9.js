4d7e93fb7a001c92ea856e9712600ba567bc9e97
GuillaumeC96 <122291329+GuillaumeC96@users.noreply.github.com>
2023-02-02 13:54:50 +0100

    Add files via upload

---
+++ 2_Bases/Partie 9/js/JS_9.js
@@ -0,0 +1,19 @@
+var a = prompt("Entrez un nombre","");
+var a = parseInt(a);
+var b = prompt("Entrez un multiplicateur","");
+var b = parseInt(b);
+
+var res = 0;
+
+function produit(x,y) 
+{
+   var res = x*y;
+   return res;
+}
+
+function afficheImg(image);
+{
+   let html = document.getElementById("img").innerHTML; 
+}
+
+
